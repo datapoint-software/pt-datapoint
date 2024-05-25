b928927304f416c888ed6ac3681d68ec3fd9d96d
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-05-25 17:49:55 +0000

    Frontend home component

--- src/angular/src/app/app.routes.ts
+++ src/angular/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { FeComponent } from './components/fe/fe.component';
+import { FeHomeComponent } from './components/fe/home/fe-home.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: FeComponent
+    component: FeComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: FeHomeComponent
+      }
+    ]
   }
 ];
---
+++ src/angular/src/app/components/fe/home/fe-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-fe-home',
+  standalone: true,
+  templateUrl: './fe-home.component.html'
+})
+export class FeHomeComponent {
+
+}
