51eaa830a4339fdc52a2b4eb5d9de038acf7d1e3
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-05-25 21:25:02 +0000

    Build and server-side rendering definitions update.

--- src/angular/server.ts
+++ src/angular/server.ts
@@ -2,23 +2,24 @@ import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
+import { basename, dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { LOCALE_ID } from '@angular/core';
+
+const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+const lang = basename(serverDistFolder);
+const browserDistFolder = resolve(serverDistFolder, `../../browser/${lang}/`);
+const indexHtml = join(serverDistFolder, 'index.server.html');
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine();
+  const server = express();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
     maxAge: '1y',
@@ -35,7 +36,10 @@ export function app(): express.Express {
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+        providers: [
+          { provide: APP_BASE_HREF, useValue: baseUrl },
+          { provide: LOCALE_ID, useValue: lang }
+        ],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
@@ -46,11 +50,13 @@ export function app(): express.Express {
 
 function run(): void {
   const port = process.env['PORT'] || 4000;
+  const server = express();
+
+  server.use(`/${lang}`, app());
 
   // Start up the Node server
-  const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`Node Express server listening on http://localhost:${port}/pt`);
   });
 }
 
